43350b61a0ec8da6b80462766925e8e7d2fa78d0
Divyansh Kushwaha <thedroiddiv@gmail.com>
2025-09-20 14:54:49 +0530

    setup multiplatform publishing

--- chitralekhan/build.gradle.kts
+++ chitralekhan/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
@@ -79,110 +80,44 @@ compose.resources {
 }
 
 
-//
-//android {
-//    namespace = "com.daiatech.chitralekhan"
-//    compileSdk = 35
-//
-//    defaultConfig {
-//        minSdk = 21
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//        consumerProguardFiles("consumer-rules.pro")
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_17
-//        targetCompatibility = JavaVersion.VERSION_17
-//    }
-//
-//    kotlinOptions.jvmTarget = "17"
-//    buildFeatures.compose = true
-//
-//    publishing {
-//        singleVariant("release") {
-//            withSourcesJar()
-//        }
-//    }
-//}
-//
-//dependencies {
-//
-//    implementation(libs.androidx.core.ktx)
-//    implementation(libs.androidx.lifecycle.runtime.ktx)
-//    implementation(libs.androidx.activity.compose)
-//    implementation(libs.androidx.appcompat)
-//
-//    implementation(platform(libs.androidx.compose.bom))
-//    implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.graphics)
-//    implementation(libs.androidx.ui.tooling.preview)
-//    implementation(libs.androidx.material3)
-//    debugImplementation(libs.androidx.ui.tooling)
-//
-//    implementation(libs.kotlinx.serialization.json)
-//}
-//
-//// Publishing tasks
-//val publishGroupId = "io.github.karya-inc"
-//val publishArtifactVersion = "0.0.4"
-//val publishArtifactId = "chitralekhan"
-//
-//group = publishGroupId
-//version = version
-//
-//publishing {
-//    publications {
-//        create<MavenPublication>("release") {
-//            groupId = publishGroupId
-//            artifactId = publishArtifactId
-//            version = publishArtifactVersion
-//
-//            afterEvaluate { from(components["release"]) }
-//
-//            pom {
-//                name.set(publishArtifactId)
-//                description.set("An Android library for image annotationo")
-//                url.set("https://github.com/karya-inc/ChitraLekhan.git")
-//
-//                licenses {
-//                    license {
-//                        name.set("GNU license")
-//                        url.set("https://opensource.org/license/gpl-3-0")
-//                    }
-//                }
-//
-//                developers {
-//                    developer {
-//                        id.set("divyansh@karya.in")
-//                        name.set("Divyansh Kushwaha")
-//                        email.set("divyansh@karya.in")
-//                    }
-//                }
-//
-//                scm {
-//                    connection.set("scm:git:ssh://git@github.com/karya-inc/ChitraLekhan.git")
-//                    developerConnection.set("scm:git:ssh://git@github.com/karya-inc/ChitraLekhan.git")
-//                    url.set("https://github.com/karya-inc/ChitraLekhan.git")
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//signing {
-//    useInMemoryPgpKeys(
-//        rootProject.ext["signing.keyId"] as String,
-//        rootProject.ext["signing.key"] as String,
-//        rootProject.ext["signing.password"] as String
-//    )
-//    sign(publishing.publications)
-//}
+group = "io.github.karya-inc"
+version = "0.0.4"
+
+mavenPublishing {
+    val artifactId = "chitralekhan"
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = artifactId,
+        version = version.toString()
+    )
+
+    pom {
+        name.set(artifactId)
+        description.set("An Android library for image annotation")
+        url.set("https://github.com/karya-inc/ChitraLekhan.git")
+
+        licenses {
+            license {
+                name.set("GNU license")
+                url.set("https://opensource.org/license/gpl-3-0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("divyansh@karya.in")
+                name.set("Divyansh Kushwaha")
+                email.set("divyansh@karya.in")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:ssh://git@github.com/karya-inc/ChitraLekhan.git")
+            developerConnection.set("scm:git:ssh://git@github.com/karya-inc/ChitraLekhan.git")
+            url.set("https://github.com/karya-inc/ChitraLekhan.git")
+        }
+    }
+}
